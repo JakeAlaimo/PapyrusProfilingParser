079403f36ec77f21e48fa382e6b6174ea153b001
Jake Alaimo <jacob.alaimo@gmail.com>
2020-06-11 18:34:33 -0400

    Adds core app functions and structure

---
+++ app.js
@@ -0,0 +1,50 @@
+let app;
+
+window.onload = () => {
+
+    app = new Vue({
+        el: '#app',
+        data: {
+          logData: {},
+          displayedData: {},
+          displayedCategory: "",
+
+        },
+        methods: {
+           Process: function() {
+              let file = document.querySelector("#file");
+
+              ProcessFile(file.files[0]).then((parsedData) => {
+                 this.logData = parsedData;  
+                 this.displayedCategory = Object.keys(this.logData)[0];
+                 this.displayedData = this.logData[this.displayedCategory];
+              });
+           },
+
+           DisplayCategory: function(e) {
+              let type = e.target.value;
+
+              for (let [key, value] of Object.entries(this.logData)) {
+                 if(key === type) {
+                    this.displayedData = value;
+                    this.displayedCategory = key;
+                 }
+              }
+           },
+
+           CopyTable: function(e) {
+              document.getSelection().removeAllRanges(); 
+
+              //create selection range and then add to selection
+              let range = new Range();
+              range.setStart(document.querySelector("table"),0);
+              range.setEnd(document.querySelector("#copyButton"),0);
+              document.getSelection().addRange(range);
+
+              document.execCommand("copy");
+           }
+        }
+    });
+}
+
+
---
+++ parser.js
@@ -0,0 +1,89 @@
+//parser.js is responsible for converting profiling logs into excel-ready data 
+
+// read and then parse the given file
+function ProcessFile(file){
+   if(!file){
+      return;
+   }
+
+   // read the file in
+   var reader = new FileReader();
+   reader.readAsText(file,'UTF-8');
+
+   //once read, parse the file
+   let parsePromise = new Promise((resolve) => {
+      reader.onload = (e) => { 
+         resolve(Parse(e.target.result));
+      };
+   });
+
+   return parsePromise;
+}
+
+// turn logs into usable data
+function Parse(fileContent)
+{
+   let lines = fileContent.split('\n');
+   let data = {};
+
+   // extract important data from each line
+   lines.forEach(line => {
+      let components = line.split(':');
+
+      if(components.length != 6){
+         return;
+      }
+
+      let type = components[5].match(/(?<=\.)(?!Register)[^\.]*$/);
+      let id = components[2];
+
+      if(!type) {
+         return;
+      }
+
+      if(!data[type]) { data[type] = {}; }
+      if(!data[type][id]) { data[type][id] = {}; }
+  
+      data[type][id][components[1]] = parseInt(components[0]);      
+   });
+
+   return data;
+}
+/*
+function SetInterface(){
+   //add copy buttons to the interface
+   let copyButtons = document.querySelector("#copyButtons");
+
+   // clear all buttons
+   var child = copyButtons.lastElementChild;  
+    while (child) { 
+        copyButtons.removeChild(child); 
+        child = copyButtons.lastElementChild; 
+    } 
+   
+   for (const type in deltaTimes) {
+
+      let li = document.createElement("li");
+
+      let copyButton = document.createElement("input");
+      copyButton.type = "button";
+      copyButton.value = "Copy " + type + " Data";
+
+      copyButton.addEventListener("click", ()=>{
+        let clipboard = document.querySelector("#clipboard");
+        clipboard.value = "";
+
+        deltaTimes[type].forEach((dt)=>{
+           clipboard.value += dt + "\n";
+        });
+
+        clipboard.select();
+        document.execCommand("copy");
+
+        alert(type + " copied to clipboard.")
+      });
+
+      li.appendChild(copyButton);
+      copyButtons.appendChild(li);
+   }
+}*/
