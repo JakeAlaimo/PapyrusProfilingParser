9fc90c3351fe6637574545d24343be3dee64783d
Jake Alaimo <jacob.alaimo@gmail.com>
2020-07-18 00:57:05 -0400

    Adds support for shared function names across scripts & aliases

--- client/src/parser.js
+++ client/src/parser.js
@@ -79,10 +79,12 @@ function CleanData(data){
       let deletedChildren = 0;
 
       for (let id of Object.keys(data[method])) {
+         //remove all cases that lack either a push or pop. without these, we lack necessary info
          data[method][id] = data[method][id].filter((instance) => {
             return instance.PUSH && instance.POP;
          });
 
+         //remove the entire branch if no instances remain
          if(data[method][id].length === 0){
             delete data[method][id];
             deletedChildren++;
@@ -98,21 +100,70 @@ function CleanData(data){
    }
 }
 
+//returns if state1 is a valid next step from state2 within the same process
+function IsNextQueueState(state1, state2) {
+
+   switch(state1){
+      case "QUEUE_PUSH":
+         return false; //queue push is always a first step
+      case "PUSH":
+         return state2 === "QUEUE_PUSH";
+      case "QUEUE_POP":
+         return state2 === "PUSH";
+      case "POP":
+         return state2 === "PUSH" || state2 === "QUEUE_POP"; //queue_pop may be skipped
+   }
+}
+
+//returns true if the given method is valid and is just using an alias (i.e.: defaultAllowPCDialogueScript.GetLevel and Actor.GetLevel)
+function IsSourceAlias(nextLine, queueState, validSource, validMethod){
+   let lookaheadComponents = nextLine.split(':');
+   if(lookaheadComponents.length != 6) {
+      return false;
+   }
+
+   let nextMethodParts = lookaheadComponents[5].trim().split('.');
+   let nextQueueState = lookaheadComponents[1];
+
+   if(nextMethodParts.pop() !== validMethod || nextMethodParts[0] !== validSource || IsNextQueueState(nextQueueState, queueState) === false){
+      return false;
+   }
+
+   return true; //next line is the appropriate method, source, and follows as the next step in the same process. this is an alias
+}
+
+//returns the new method name in the [script].[method] format when disambiguating
+function AppendScriptName(method, callingScript, nextLine, queueState){
+   let newName = "";
+   //if possible, use the script associated w. next line (to prevent aliases from being split off)
+   if(nextLine.length === 6 && IsNextQueueState(nextLine[1], queueState)) { 
+      newName = nextLine[5].trim().split('.')[0] + '.' + method;
+   } else {
+      newName = callingScript + '.' + method;
+   }
+
+   return newName;
+}
+
 // turn logs into usable data
 function Parse(fileContent)
 {
    let lines = fileContent.split('\n');
    let data = {"Stack Timings": {}};
 
-   // extract important data from each line
-   lines.forEach(line => {
-      let components = line.split(':');
+   let methodSources = {}; //pairs each method name with it's script source (to distinguish functions sharing names)
 
+   // extract important data from each line
+   lines.forEach((line, i) => {
+      let components = line.split(':');
       if(components.length != 6) {
          return;
       }
 
-      let method = components[5].trim().split('.').pop();
+      let methodDataParts = components[5].trim().split('.');
+      let callingScript = methodDataParts[0]; //the name of the script that called the method
+      let method = methodDataParts.pop(); //the name of the method
+
       let id = components[2];
       let queueState = components[1];
       let time = components[0];
@@ -125,6 +176,22 @@ function Parse(fileContent)
          data["Stack Timings"][id] = [{PUSH: time}];
       }
       data["Stack Timings"][id][0].POP = time;
+
+      let nextLine = (i+1 < lines.length) ? lines[i+1] : "";
+      let nextLineSplit = nextLine.split(':');
+
+      //expand the method name if different sources have methods of the same name
+      if(methodSources[method] === undefined && nextLineSplit.length === 6 && IsNextQueueState(nextLineSplit[1], queueState) === false) {
+         methodSources[method] = {source: callingScript, multipleSources: false, lastQueueState: queueState};
+      } else if (methodSources[method] !== undefined) {
+         if(methodSources[method].multipleSources === true) { 
+            method = AppendScriptName(method, callingScript, nextLineSplit, queueState); //disambiguate by appending script name to method
+         } else if(methodSources[method].source !== callingScript && IsSourceAlias(lines[i+1], queueState, methodSources[method].source, method) === false) {
+            methodSources[method].multipleSources = true;
+            delete Object.assign(data, {[methodSources[method].source + '.' + method]: data[method] })[method]; //rename [method] to [source.method]
+            method = AppendScriptName(method, callingScript, nextLineSplit, queueState);
+         }
+      }
 
       //ensure the data store has a place to put this bit
       if(!data[method]) { data[method] = {}; }
@@ -139,7 +206,7 @@ function Parse(fileContent)
          methodInstances.push({[queueState]: time});
       } else {
          lastMethodInstance[queueState] = time; //add data to current method instance
-      } 
+      }
    });
 
    //remove incomplete data points from the set
